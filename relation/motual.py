175f9e4e29693262066bf8a75456e31c0e765493
pufaqi <1094447564@qq.com>
2019-01-02 09:07:04 +0800

    Initial commit

---
+++ relation/motual.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author:loftybay
+username = int (input("username:"))
+#password = input("password:")
+
+
+info = '''
+----- info of  %s ----
+Username: %s 
+#Password: %s
+'''  % (username)
+#print(info)
---
+++ relation/relation1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author:loftybay
+'''
+name_file = "pufaqi"
+print("hello world" )
+'''
+#print ("my name is ", name_file)
+print("你好！世界！")
+print('i"m pufaqi')
