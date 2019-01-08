e38fb22e0efd87a029165d6acf05d426a10dfe02
pufaqi <1094447564@qq.com>
2019-01-08 17:22:25 +0800

    练习

--- exercise/longo.py
+++ exercise/longo.py
@@ -13,36 +13,58 @@ def mkdir_dir(path):
         files = open('userinfo\\user_lock' + '.txt','w')
         files.close()
 
+userinfo_user = 'userinfo\\user_name.txt'
+userinfo_pass = 'userinfo\\user_passwd.txt'
+userinfo_lock = 'userinfo\\user_lock.txt'
+
+#检查用户名是否被锁定函数
+def _CHECK_USER_LOCK():
+    lock_count = 0
+    while lock_count < 3:
+        username = input("请输入你的用户名：")
+        password = input("请输入你的密码：")
+        lock_user = open(userinfo_lock,'r+')
+        lock_list = lock_user.readlines()
+        lock_user.close()
+        for lock_line in lock_list:
+            lock_line = lock_line.strip('\n')
+            if username == lock_line:
+                print("用户被锁定，请联系管理员！")
+                lock_count = 3
+                break
+            else:
+                lock_count = 3
+
+#检查用户是否存在目录中函数
+def _CHECK_USER_NAME():
+    lock_count = 0
+    while lock_count < 3:
+        username = input("请输入你的用户名：")
+        password = input("请输入你的密码：")
+        lock_user = open(userinfo_lock,'r+')
+        lock_list = lock_user.readlines()
+        lock_user.close()
+        for lock_line in lock_list:
+            lock_line = lock_line.strip('\n')
+            if username == lock_line:
+                print("用户被锁定，请联系管理员！")
+                lock_count = 3
+                break
+            else:
+                lock_count = 3
+#创建目录和文件
 file = 'userinfo'
 mkdir_dir(file)  # 调用函数
 
-longo_count = 0
-while longo_count < 3:
-    select = input("请选择登录（1）或者注册（2）：")
-    if select == "1":
-        lock_count = 0
-        while lock_count < 3:
-            username = input("请输入你的用户名：")
-            password = input("请输入你的密码：")
-            userinfo_user = 'userinfo\\user_name.txt'
-            userinfo_pass = 'userinfo\\user_passwd.txt'
-            userinfo_lock = 'userinfo\\user_lock.txt'
-            lock_user = open(userinfo_lock,'r+')
-            lock_list = lock_user.readlines()
-            for lock_line in lock_list:
-                lock_line = lock_line.strip('\n')
-                if username == lock_list:
-                    sys.exit('用户 %s 已经被锁定，退出' % username)
-            lock_user.close()
-        else:
-            print("用户被锁，请联系管理员！")
-            break
-        longo_count = longo_count +1
-    elif select == "2":
-        username = input("请输入你要注册的用户名：")
-        passwordA = input("请输入你的密码：")
-        passwordB = input("请再次输入你的密码：")
-        longo_count = 3
-    else:
-        print("输入无效：")
-        longo_count = longo_count +1
+#代码开始
+select = input("请选择登录（1）或者注册（2）：")
+_CHECK_USER_LOCK()
+print("kaishi")
+
+''''
+elif select == "2":
+username = input("请输入你要注册的用户名：")
+passwordA = input("请输入你的密码：")
+passwordB = input("请再次输入你的密码：")
+longo_count = 3
+'''
