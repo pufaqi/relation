4f662544afdf0d9b9b3491ddb29df8ac5dc2d29f
pufaqi <1094447564@qq.com>
2019-01-09 17:47:19 +0800

    本次的提交，重构了代码。将原先的判断拆分为函数！

--- exercise/longo.py
+++ exercise/longo.py
@@ -16,26 +16,31 @@ def mkdir_dir(path):
 userinfo_user = 'userinfo\\user_name.txt'
 userinfo_pass = 'userinfo\\user_passwd.txt'
 userinfo_lock = 'userinfo\\user_lock.txt'
-
-#检查用户名是否被锁定函数
+#检查文件函数
+def _CHECK_USERINFO(file_name):
+    global check_line
+    check_user = open(file_name,'r+')
+    check_list = check_user.readline()
+    check_user.close()
+    for check_line in check_list:
+        check_line = check_line.strip('\n')
+        print(check_line)
+##检查用户是否被锁定
 def _CHECK_USER_LOCK():
     lock_count = 0
     while lock_count < 3:
         username = input("请输入你的用户名：")
         password = input("请输入你的密码：")
-        lock_user = open(userinfo_lock,'r+')
-        lock_list = lock_user.readlines()
-        lock_user.close()
-        for lock_line in lock_list:
-            lock_line = lock_line.strip('\n')
-            if username == lock_line:
-                print("用户被锁定，请联系管理员！")
-                lock_count = 3
-                break
-            else:
-                lock_count = 3
-
-#检查用户是否存在目录中函数
+        file_name = "userinfo\\user_lock.txt"
+        _CHECK_USERINFO(file_name)
+        print(check_line)
+        if username == check_line:
+            print("用户已经被锁定，请联系管理员！")
+            lock_count = 3
+            break
+        else:
+            lock_count = 3
+#检查用户是否存在函数
 def _CHECK_USER_NAME():
     lock_count = 0
     while lock_count < 3:
@@ -52,6 +57,20 @@ def _CHECK_USER_NAME():
                 break
             else:
                 lock_count = 3
+'''
+#注册用户函数
+def _USER_REGISTER():
+    register_count = 0
+    while register_count < 3:
+        username = input("请输入你要注册的用户名")
+        check_user = open(userinfo_user,'r+')
+        check_list = check_user.readline()
+        check_user.close()
+        for check_line in check_list:
+            check_line = check_line.strip('\n')
+            if userename
+'''
+
 #创建目录和文件
 file = 'userinfo'
 mkdir_dir(file)  # 调用函数
